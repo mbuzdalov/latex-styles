20213b05a8d04a79551aa6f1104331c27eac6217
Maxim Buzdalov <mbuzdalov@gmail.com>
2017-06-07 01:50:57 +0300

    Version b5 with mostly cosmetics. FAQs updated.

--- itmo-student-thesis/r.sh
+++ itmo-student-thesis/r.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-for i in bachelor master; do
-	xelatex $i-thesis
-	biber   $i-thesis
-	xelatex $i-thesis
-	xelatex $i-thesis
-done
+if [[ "$1" == "clean" ]]; then
+    rm -f {bachelor,master}-thesis{,-legacy}.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,pdf,out}
+else
+    for i in bachelor master; do
+        xelatex $i-thesis
+        biber   $i-thesis
+        xelatex $i-thesis
+        xelatex $i-thesis
+    done
 
-for i in bachelor; do
-    pdflatex $i-thesis-legacy
-    biber    $i-thesis-legacy
-    pdflatex $i-thesis-legacy
-    pdflatex $i-thesis-legacy
-done
+    for i in bachelor; do
+        pdflatex $i-thesis-legacy
+        biber    $i-thesis-legacy
+        pdflatex $i-thesis-legacy
+        pdflatex $i-thesis-legacy
+    done
 
-rm -f {bachelor,master}-thesis{,-legacy}.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct}
+    rm -f {bachelor,master}-thesis{,-legacy}.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,out}
+fi
