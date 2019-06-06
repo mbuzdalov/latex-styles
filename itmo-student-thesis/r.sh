5cb5a7a192fc8dfb32d013a09d07bdbb54e35dfe
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-06-06 22:19:34 +0300

    Implement bilingual auxiliary docs. Closes #29.

--- itmo-student-thesis/r.sh
+++ itmo-student-thesis/r.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ "$1" == "clean" ]]; then
-    rm -f {bachelor,master}-thesis.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,pdf,out}
+    rm -f {bachelor-thesis,master-thesis,master-thesis-en}.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,pdf,out}
 else
-    for i in bachelor master; do
-        xelatex $i-thesis
-        biber   $i-thesis
-        xelatex $i-thesis
-        xelatex $i-thesis
+    for i in bachelor-thesis master-thesis master-thesis-en; do
+        xelatex $i
+        biber   $i
+        xelatex $i
+        xelatex $i
     done
 
-    rm -f {bachelor,master}-thesis.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,out}
+    rm -f {bachelor-thesis,master-thesis,master-thesis-en}.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,out}
 fi
